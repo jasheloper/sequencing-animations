caf28681375daa5f78304f85bbf14dedd652d1fc
Jashele T. <48870048+jasheloper@users.noreply.github.com>
2024-10-02 16:59:02 -0700

    callback hell approach
    First, implement something that works, but has the promise version of the "callback hell" problem we saw in our discussion of using callbacks.
    
    https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Sequencing_animations

--- main.js
+++ main.js
@@ -3,14 +3,34 @@ const aliceTumbling = [
   { transform: "rotate(360deg) scale(0)" },
 ];
 
+console.log(aliceTumbling);
+
 const aliceTiming = {
   duration: 2000,
   iterations: 1,
   fill: "forwards",
 };
 
+console.log(aliceTiming);
+
 const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
 
-alice1.animate(aliceTumbling, aliceTiming);
+/*
+animate alice2 when alice1 has finished, and alice3 when alice2 has finished.
+
+The animate() method returns an Animation object.
+
+This object has a finished property, which is a Promise that is fulfilled when the animation has finished playing. So we can use this promise to know when to start the next animation.
+*/
+
+/*
+First, implement something that works, but has the promise version of the "callback hell" problem we saw in our discussion of using callbacks.
+*/
+
+alice1.animate(aliceTumbling, aliceTiming).finished.then( () => {
+  alice2.animate(aliceTumbling, aliceTiming).finished.then( () => {
+    alice3.animate(aliceTumbling, aliceTiming);
+  })
+});
