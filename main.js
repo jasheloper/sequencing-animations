48b6907f61a84c47af31812ccdb56e7beed61dc1
Jashele T. <48870048+jasheloper@users.noreply.github.com>
2024-10-02 17:33:16 -0700

    refactor using promise chain
    Next, implement it as a promise chain. Note that there are a few different ways you can write this, because of the different forms you can use for an arrow function. Try some different forms. Which is the most concise? Which do you find the most readable?
    
    https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Sequencing_animations

--- main.js
+++ main.js
@@ -26,11 +26,13 @@ This object has a finished property, which is a Promise that is fulfilled when t
 */
 
 /*
-First, implement something that works, but has the promise version of the "callback hell" problem we saw in our discussion of using callbacks.
+approach 2: promise chain 
+
+Next, implement it as a promise chain. Note that there are a few different ways you can write this, because of the different forms you can use for an arrow function. Try some different forms. Which is the most concise? Which do you find the most readable?
 */
 
-alice1.animate(aliceTumbling, aliceTiming).finished.then( () => {
-  alice2.animate(aliceTumbling, aliceTiming).finished.then( () => {
-    alice3.animate(aliceTumbling, aliceTiming);
-  })
-});
+alice1.animate(aliceTumbling, aliceTiming).finished
+ .then(() => { return alice2.animate(aliceTumbling, aliceTiming).finished })
+ .then(() => { return alice3.animate(aliceTumbling, aliceTiming)});
+
+
